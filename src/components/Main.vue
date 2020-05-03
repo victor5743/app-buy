04c4a071496fce66554d7b4baa2e6b1760d8b67b
Victor <vcs5743@gmail.com>
2020-05-02 23:03:11 -0400

    Second section finished
    Responsive settings added

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,8 +4,9 @@
 		<Navbar />
 
 		<!-- Checkout -->
-		<div class="block sm:flex mt-16 mx-auto max-w-2xl rounded-lg shadow-lg px-8 py-8">
-			<div class="w-2/3 pr-8">
+		<div class="block p-8 sm:p-0 sm:flex my-16 mx-auto max-w-3xl rounded-lg shadow-lg">
+			<!-- Left panel -->
+			<div class="w-full sm:w-2/3 p-8">
 				<form action="">
 					<h1 class="border-b pb-8 text-xl text-gray-800">Checkout</h1>
 					<div class="pt-8 border-t border-gray-400">
@@ -17,8 +18,8 @@
 							<h2 class="text-">Your Basic Information</h2>
 						</div>
 						<div class="block">
-							<div class="flex">
-								<div class="w-1/2 mb-2 mr-1">
+							<div class="block sm:flex">
+								<div class="w-full sm:w-1/2 mb-2 sm:mr-1">
 									<label class="block text-sm mb-2 text-gray-800" for="first_name"
 										>First Name</label
 									>
@@ -29,7 +30,7 @@
 										placeholder="Jone"
 									/>
 								</div>
-								<div class="w-1/2 mb-2 ml-1">
+								<div class="w-full sm:w-1/2 mb-2 sm:ml-1">
 									<label class="block text-sm mb-2 text-gray-800" for="last_name"
 										>Last Name</label
 									>
@@ -106,13 +107,72 @@
 						</div>
 						<!-- End Second Section -->
 					</div>
-					<!-- End Panel -->
 				</form>
 			</div>
-			<div class="w-1/3 bg-primary">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ipsa mollitia
-				ducimus? Id voluptatum officia, harum ducimus quas alias labore quisquam
-				accusantium quod aliquam ea, eius optio minima aspernatur explicabo.
+			<!-- End Panel -->
+
+			<!-- Right panel -->
+			<div class="block w-full sm:w-1/3 bg-primary p-8 text-white">
+				<h2 class="font-semibold text-xl mb-4 text-white">Freelancer</h2>
+				<span class="text-3xl">$</span>
+				<span class="text-5xl font-light">99</span>
+				<span class="text-lg font-light">/ year</span>
+				<div class="w-3/4 italic leading-normal mb-8">
+					Automatically renews after 1 year
+				</div>
+				<div>
+					<ul>
+						<li class="mb-4 flex items-center">
+							<svg
+								class="h-4 w-4 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+							>
+								<path
+									d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z"
+								/>
+							</svg>
+							<span class="ml-2">Use on 5 client sites</span>
+						</li>
+						<li class="mb-4 flex items-center">
+							<svg
+								class="h-4 w-4 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+							>
+								<path
+									d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z"
+								/>
+							</svg>
+							<span class="ml-2">Private Repositories</span>
+						</li>
+						<li class="mb-8 flex items-center">
+							<svg
+								class="h-4 w-4 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+							>
+								<path
+									d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z"
+								/>
+							</svg>
+							<span class="ml-2">Email Support</span>
+						</li>
+					</ul>
+					<!-- End list items -->
+					<div class="border-t-2 border-white py-8">
+						<span>
+							FTP is horrible to deal with. Vue App solves that issue, and the customer
+							support is awesome!
+						</span>
+						<img
+							class="w-24 h-24 object-cover rounded-full mx-auto mt-8"
+							src="../assets/24998.jpg"
+							alt="Avatar"
+							srcset=""
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
